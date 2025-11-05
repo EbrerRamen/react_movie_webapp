e7c4c22473c5dd15e3d71a8833ffcca9891792bb
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-11-05 18:05:34 +0600

    Spinner loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Search from "./components/Search.jsx";
+import Spinner from "./components/Spinner.jsx";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -65,13 +66,13 @@ const App = () => {
                 <section className= "all-movies">
                     <h2>All Movies</h2>
                     {isLoading ? (
-                        <p className={"text-white"}>Loading...</p>
+                        <Spinner />
                     ) : errorMessage ? (
                         <p className={"text-red-500"}>{errorMessage}</p>
                     ) : (
                         <ul>
                             {movieList.map((movie) => (
-                                <p className={"text-white"}>{movie.title}</p>
+                                <p key={movie.id} className={"text-white"}>{movie.title}</p>
                             ))}
                         </ul>
                     )}
