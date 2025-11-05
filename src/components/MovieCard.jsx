4f52b308b7d154334c6183f68124d9e47d7c2bf7
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-11-05 19:07:23 +0600

    Fetching movie title,poster, and rating using movie card component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,6 +6,12 @@ const MovieCard = ({ movie: {title, vote_average, poster_path, release_date, ori
             <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} alt={title}/>
             <div className={"mt-4"}>
                 <h3>{title}</h3>
+                <div className={"content"}>
+                    <div className={"rating"}>
+                        <img src={"star.svg"} alt={"Star Icon"} />
+                        <p>{vote_average ? vote_average.toFixed( 1) : 'N/A'}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
