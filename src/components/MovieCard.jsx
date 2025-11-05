1042e3b0864aec2f3bce1a204424e80b03ec5131
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-11-05 19:10:46 +0600

    Added release year in movie card. Movie Card Complete

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -15,6 +15,11 @@ const MovieCard = ({ movie: {title, vote_average, poster_path, release_date, ori
                     <span>•</span>
                     <p className={"lang"}>{original_language}</p>
 
+                    <span>•</span>
+                    <p className={"year"}>
+                        {release_date ? release_date.split('-')[0] : 'N/A'}
+                    </p>
+
                 </div>
             </div>
         </div>
